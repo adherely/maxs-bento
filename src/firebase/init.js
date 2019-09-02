e9d0dfe32cc9ddb732088641fde5ec4099242ff8
Max-Behling <elmachomax123@gmail.com>
2019-09-02 13:04:24 -0700

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,13 @@ import 'firebase/auth'
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-  apiKey: 'AIzaSyCe6CSzOUs6_Ij_VHa6hancdEi6CNBtw7c',
-  authDomain: 'bento-starter.firebaseapp.com',
-  databaseURL: 'https://bento-starter.firebaseio.com',
-  projectId: 'bento-starter',
-  storageBucket: 'bento-starter.appspot.com',
-  messagingSenderId: '458464977217'
-}
+  apiKey: "AIzaSyABGiaFVjY9zChu12o6CjW4QAYjUOacsCA",
+  authDomain: "new-adherely-2e51d.firebaseapp.com",
+  databaseURL: "https://new-adherely-2e51d.firebaseio.com",
+  projectId: "new-adherely-2e51d",
+  storageBucket: "new-adherely-2e51d.appspot.com",
+  messagingSenderId: "638226919184",
+  appId: "1:638226919184:web:5e29c923d0d6d332"
+};
 
 firebase.initializeApp(config)
